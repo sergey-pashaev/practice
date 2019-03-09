d88cfa00162fdff9a3839167254bcffc712edb89
Sergey Pashaev <sergey.pashaev@gmail.com>
2019-03-09 12:50:01 +0400

    cpp: project structure change
    Move all common for c++ projects stuff to cpp/:
    - build scripts
    - thirdparty libraries
    - .{ag,git}ignore, clang format configs, etc.

--- cpp/ciu/scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-IFS=$'\n\t'
-
-mkdir -p build/ && cd build/
-CC=clang CXX=clang++ cmake $@ ..
-make -j4
-cd ../
--- cpp/ciu/scripts/tests.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-IFS=$'\n\t'
-
-./scripts/build.sh $@
-cd build/
-ctest -V .
