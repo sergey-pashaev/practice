ded80be4110b29003ac82d36226494c90a3d654f
Sergey Pashaev <sergey.pashaev@gmail.com>
2019-04-25 09:46:42 +0400

    design: add interface adapter

---
+++ cpp/design/src/interface_adapter.cpp
@@ -0,0 +1,35 @@
+#include <catch.hpp>
+
+#include <iostream>
+#include <memory>
+
+class Interface {
+   public:
+    virtual ~Interface() {}
+    virtual int Function() = 0;
+};
+
+template <class T, class P>
+std::unique_ptr<Interface> MakeAdapter(const T& obj, const P& arg) {
+    class Local : public Interface {
+       public:
+        Local(const T& obj, const P& arg) : obj_{obj}, arg_{arg} {}
+        int Function() override { return obj_(arg_); }
+
+       private:
+        T obj_;
+        P arg_;
+    };
+
+    return std::make_unique<Local>(obj, arg);
+}
+
+struct Foo {
+    int operator()(int v) { return v; }
+};
+
+TEST_CASE("interface adapter") {
+    Foo foo;
+    auto adapter = MakeAdapter(foo, 5);
+    REQUIRE(adapter->Function() == 5);
+}
