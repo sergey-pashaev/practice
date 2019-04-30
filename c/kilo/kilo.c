57bd4a0bc195905d8a7426efcbaac33da3bf3754
Sergey Pashaev <sergey.pashaev@gmail.com>
2019-04-30 18:51:13 +0400

    kilo: draw tildas lines

--- c/kilo/kilo.c
+++ c/kilo/kilo.c
@@ -12,7 +12,10 @@
 /* globals */
 struct termios orig_termios;
 
+void clear_screen();
+
 void die(const char* msg) {
+    clear_screen();
     perror(msg);
     exit(1);
 }
@@ -66,6 +69,7 @@ void editor_process_keypress() {
     char c = editor_read_key();
     switch (c) {
         case CTRL_KEY('q'): {
+            clear_screen();
             exit(0);
             break;
         }
@@ -74,10 +78,37 @@ void editor_process_keypress() {
     }
 }
 
+/* output */
+
+void editor_draw_rows() {
+    for (int y = 0; y < 24; ++y) {
+        write(STDIN_FILENO, "~\r\n", 3);
+    }
+}
+
+void editor_refresh_screen() {
+    clear_screen();
+    editor_draw_rows();
+    /* position cursor at first row & column */
+    write(STDOUT_FILENO, "\x1b[1;1H", 6);
+}
+
+void clear_screen() {
+    /* \x1b[ = 27 + [ = escape sequence */
+    /* J = erase in display */
+    /* 2 = argument for erase command */
+    /* https://vt100.net/docs/vt100-ug/chapter3.html#ED */
+    write(STDIN_FILENO, "\x1b[2J", 4);
+
+    /* position cursor at first row & column */
+    write(STDOUT_FILENO, "\x1b[1;1H", 6);
+}
+
 int main(int argc, char* argv[]) {
     enable_raw_mode();
 
     while (1) {
+        editor_refresh_screen();
         editor_process_keypress();
     }
 
