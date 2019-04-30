4a974a011d099b6a9a1056880e816ccf4c4e0962
Sergey Pashaev <sergey.pashaev@gmail.com>
2019-04-30 19:23:24 +0400

    kilo: add get_cursor_position function

--- c/kilo/kilo.c
+++ c/kilo/kilo.c
@@ -23,6 +23,7 @@ void enable_raw_mode();
 
 /* defines */
 #define CTRL_KEY(k) ((k)&0x1f)
+#define TRAILING_ZERO 1
 
 /* globals */
 struct editor_config_t {
@@ -43,11 +44,32 @@ void die(const char* msg) {
 int get_window_size(int* rows, int* cols) {
     struct winsize ws;
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-        return -1;
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        return get_cursor_position(rows, cols);
     }
 
     *rows = ws.ws_row;
     *cols = ws.ws_col;
+    return 0;
+}
+
+int get_cursor_position(int* row, int* col) {
+    char buf[32];
+    unsigned int i = 0;
+
+    /* https://vt100.net/docs/vt100-ug/chapter3.html#CPR */
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    while (i < sizeof(buf) - TRAILING_ZERO) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        ++i;
+    }
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", row, col) != 2) return -1;
+
     return 0;
 }
 
