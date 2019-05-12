5c55897afa2538833ce3dd4fb2c7283aeb8d7dee
Sergey Pashaev <sergey.pashaev@gmail.com>
2019-05-12 19:42:46 +0400

    editor: open scratch buffer if no file supplied

--- cpp/editor/src/app.cpp
+++ cpp/editor/src/app.cpp
@@ -3,23 +3,16 @@
 #include <editor/editor.hpp>
 #include <ncpp/ncurses.hpp>
 
-int usage(const char* bin) {
-    std::cout << "Usage: " << bin << " <file>\n";
-    return 1;
-}
-
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        return usage(argv[0]);
-    }
-
     auto& nc = ncpp::ncurses();
     nc.SetRaw(true);
     nc.SetKeyPad(true);
     nc.SetEcho(false);
 
     editor::Editor e;
-    e.Open(argv[1]);
+    if (argc > 1) {
+        e.Open(argv[1]);
+    }
     e.Run();
     return 0;
 }
