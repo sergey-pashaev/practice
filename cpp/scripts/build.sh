b146c478f5ccc2ac7da0f57550c3297ee1dea6a4
Sergey Pashaev <sergey.pashaev@gmail.com>
2019-06-04 09:39:09 +0400

    cpp: use conan package manager

--- cpp/scripts/build.sh
+++ cpp/scripts/build.sh
@@ -2,7 +2,12 @@
 set -Eeuo pipefail
 IFS=$'\n\t'
 
+# magic line to ensure that we're always inside the root of our repo,
+# no matter from which directory we'll run script
+cd "${0%/*}/.."
+
 mkdir -p build/ && cd build/
-CC=clang CXX=clang++ cmake $@ ..
-make -j4
+conan install ..
+cmake $@ ..
+cmake --build .
 cd ../
