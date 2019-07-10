700b868127ae55ae1140a42c066a248d81d6e82b
Sergey Pashaev <sergey.pashaev@gmail.com>
2019-07-10 10:10:29 +0400

    cpp/adt: add ternary search tree (tst) implementation

---
+++ cpp/adt/src/tst.cpp
@@ -0,0 +1,23 @@
+#include <catch2/catch.hpp>
+
+#include "tst.h"
+
+TEST_CASE("tst") {
+    psv::TernarySearchTree tst;
+    tst.Insert("hello");
+    tst.Insert("hell");
+    tst.Insert("jello");
+    tst.Insert("hi");
+    tst.Insert("hid");
+
+    REQUIRE(tst.Search("hi"));
+    REQUIRE(tst.Search("hello"));
+    REQUIRE(tst.Search("hell"));
+    REQUIRE(tst.Search("hello"));
+    REQUIRE(tst.Search("jello"));
+    REQUIRE(tst.Search("hid"));
+
+    REQUIRE(!tst.Search("hida"));
+    REQUIRE(!tst.Search("h"));
+    REQUIRE(!tst.Search("not"));
+}
---
+++ cpp/adt/src/tst.h
@@ -0,0 +1,99 @@
+#include <string>
+
+namespace psv {
+
+class TernarySearchTree {
+   private:
+    struct Node {
+        Node(int d) : data{d} {}
+
+        int data;
+        bool end = false;
+        Node* lt = nullptr;
+        Node* eq = nullptr;
+        Node* gt = nullptr;
+    };
+
+    using Link = Node*;
+
+   public:
+    TernarySearchTree() = default;
+    ~TernarySearchTree() {
+        if (root_) {
+            Clean(root_);
+            delete root_;
+        }
+    }
+
+    void Clean(Link node) {
+        if (node->lt) {
+            Clean(node->lt);
+            delete node->lt;
+        }
+
+        if (node->gt) {
+            Clean(node->gt);
+            delete node->gt;
+        }
+
+        if (node->eq) {
+            Clean(node->eq);
+            delete node->eq;
+        }
+    }
+
+    bool Search(const std::string& key) { return SearchR(key, 0, root_); }
+
+    void Insert(const std::string& key) { InsertR(key, 0, root_); }
+
+   private:
+    static const int eok = -1;  // end of key
+
+    int digit(const std::string& key, std::size_t pos) {
+        return pos == key.size() ? eok : key[pos];
+    }
+
+    bool SearchR(const std::string& key, std::size_t pos, Link node) {
+        int i = digit(key, pos);
+        if (!node) {
+            return false;
+        }
+
+        if (i == eok) {
+            return node->end;
+        }
+
+        if (i < node->data) {
+            return SearchR(key, pos, node->lt);
+        } else if (i > node->data) {
+            return SearchR(key, pos, node->gt);
+        } else {
+            return SearchR(key, pos + 1, node->eq);
+        }
+    }
+
+    void InsertR(const std::string& key, std::size_t pos, Link& node) {
+        int i = digit(key, pos);
+        if (node == nullptr) {
+            node = new Node(i);
+        }
+
+        if (i == eok) {
+            node->end = true;
+            return;
+        }
+
+        if (i < node->data) {
+            InsertR(key, pos, node->lt);
+        } else if (i == node->data) {
+            InsertR(key, pos + 1, node->eq);
+        } else {
+            InsertR(key, pos, node->gt);
+        }
+    }
+
+   private:
+    Link root_ = nullptr;
+};
+
+}  // namespace psv
