9878ede4bace4b4742107ef4cd4719e25d3ad5dc
Sergey Pashaev <sergey.pashaev@gmail.com>
2020-04-04 15:07:21 +0300

    cpp/dbg: add comments & link on register names & order

--- cpp/dbg/src/registers.cpp
+++ cpp/dbg/src/registers.cpp
@@ -6,35 +6,41 @@
 #include <sys/ptrace.h>
 #include <sys/user.h>  // user_regs_struct
 
+// clang-format off
+
+// See /usr/include/x86_64-linux-gnu/sys/user.h for struct user_regs_struct &
+// registers order. See https://www.uclibc.org/docs/psABI-x86_64.pdf for DWARF
+// register numbers.
 const std::array<RegisterDescriptor, kRegistersCount> kRegisterDescriptors = {{
-    {Register::r15, 15, "r15"},
-    {Register::r14, 14, "r14"},
-    {Register::r13, 13, "r13"},
-    {Register::r12, 12, "r12"},
-    {Register::rbp, 6, "rbp"},
-    {Register::rbx, 3, "rbx"},
-    {Register::r11, 11, "r11"},
-    {Register::r10, 10, "r10"},
-    {Register::r9, 9, "r9"},
-    {Register::r8, 8, "r8"},
-    {Register::rax, 0, "rax"},
-    {Register::rcx, 2, "rcx"},
-    {Register::rdx, 1, "rdx"},
-    {Register::rsi, 4, "rsi"},
-    {Register::rdi, 5, "rdi"},
-    {Register::orig_rax, -1, "orig_rax"},
-    {Register::rip, -1, "rip"},
-    {Register::cs, 51, "cs"},
-    {Register::rflags, 49, "eflags"},
-    {Register::rsp, 7, "rsp"},
-    {Register::ss, 52, "ss"},
-    {Register::fs_base, 58, "fs_base"},
-    {Register::gs_base, 59, "gs_base"},
-    {Register::ds, 53, "ds"},
-    {Register::es, 50, "es"},
-    {Register::fs, 54, "fs"},
-    {Register::gs, 55, "gs"},
+    {Register::r15,       15, "r15"},
+    {Register::r14,       14, "r14"},
+    {Register::r13,       13, "r13"},
+    {Register::r12,       12, "r12"},
+    {Register::rbp,       6,  "rbp"},
+    {Register::rbx,       3,  "rbx"},
+    {Register::r11,       11, "r11"},
+    {Register::r10,       10, "r10"},
+    {Register::r9,        9,  "r9"},
+    {Register::r8,        8,  "r8"},
+    {Register::rax,       0,  "rax"},
+    {Register::rcx,       2,  "rcx"},
+    {Register::rdx,       1,  "rdx"},
+    {Register::rsi,       4,  "rsi"},
+    {Register::rdi,       5,  "rdi"},
+    {Register::orig_rax, -1,  "orig_rax"},
+    {Register::rip,      -1,  "rip"},
+    {Register::cs,       51,  "cs"},
+    {Register::rflags,   49,  "eflags"},
+    {Register::rsp,      7,   "rsp"},
+    {Register::ss,       52,  "ss"},
+    {Register::fs_base,  58,  "fs_base"},
+    {Register::gs_base,  59,  "gs_base"},
+    {Register::ds,       53,  "ds"},
+    {Register::es,       50,  "es"},
+    {Register::fs,       54,  "fs"},
+    {Register::gs,       55,  "gs"},
 }};
+// clang-format on
 
 namespace {
 
