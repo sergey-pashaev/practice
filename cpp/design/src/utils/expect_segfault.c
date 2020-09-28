8f00670492cec4c372d130083f9ea63c16ab920e
Sergey Pashaev <sergey.pashaev@gmail.com>
2020-09-28 22:34:41 +0300

    cpp/design: make expect_segfault more verbose

--- cpp/design/src/utils/expect_segfault.c
+++ cpp/design/src/utils/expect_segfault.c
@@ -1,11 +1,19 @@
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        // Not enough arguments means failure.
+        printf("Usage: %s executable-file [inferior-arguments ...]\n", argv[0]);
+        return 1;
+    }
     pid_t pid = fork();
     if (pid == -1) {
         // Fork failed.
+        fprintf(stderr, "%s: Fork failed.\n", argv[0]);
         return 1;
     } else if (pid) {
         // Parent process.
@@ -16,6 +24,9 @@ int main(int argc, char *argv[]) {
             return 0;
         } else {
             // Not terminated means failure.
+            fprintf(stderr,
+                    "%s: Inferior process is not terminated by signal.\n",
+                    argv[0]);
             return 1;
         }
     } else {
