dfd7408b1b5cbe33ae8c14fa6d6b79032764851d
Sergey Pashaev <sergey.pashaev@gmail.com>
2020-10-10 22:47:31 +0300

    cpp/design: add chapter.3.6

---
+++ cpp/design/src/chapter-3/chapter.3.6.cpp
@@ -0,0 +1,21 @@
+#include <catch2/catch.hpp>
+
+#include <type_traits>
+
+#include <chapter-3/typelist.h>
+
+namespace c_3_2 {
+
+namespace {
+
+TEST_CASE("TypeAt") {
+    REQUIRE(std::is_same<TypeAt<TYPELIST_1(int), 0>::Result, int>::value);
+    REQUIRE(std::is_same<TypeAt<TYPELIST_3(int, char, double), 1>::Result,
+                         char>::value);
+    REQUIRE(std::is_same<TypeAt<TYPELIST_3(int, char, double), 2>::Result,
+                         double>::value);
+}
+
+}  // namespace
+
+}  // namespace c_3_2
--- cpp/design/src/chapter-3/typelist.h
+++ cpp/design/src/chapter-3/typelist.h
@@ -18,8 +18,23 @@ struct Length<NullType> {
     enum { value = 0 };
 };
 
-template <class T, class U> struct Length<TypeList<T, U>> {
+template <class T, class U>
+struct Length<TypeList<T, U>> {
     enum { value = 1 + Length<U>::value };
+};
+
+// TypeAt
+template <class TList, unsigned int index>
+struct TypeAt;
+
+template <class Head, class Tail>
+struct TypeAt<TypeList<Head, Tail>, 0> {
+    using Result = Head;
+};
+
+template <class Head, class Tail, unsigned int i>
+struct TypeAt<TypeList<Head, Tail>, i> {
+    using Result = typename TypeAt<Tail, i - 1>::Result;
 };
 
 #define TYPELIST_1(T1) TypeList<T1, NullType>
